7c270789d5ce70ecc01566a0cee93632e263c816
Gary Chen <garychen.whc@gmail.com>
2019-10-14 20:29:50 +0000

    tried to set up validaiton for store, will finish it up later

--- lib/store.rb
+++ lib/store.rb
@@ -2,4 +2,11 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true }, presence: true
+  # validate :validate_apparel
+
+  # def validate_apparel
+  #   if mens_apparel == false
+  #     errors.add(:mens_apparel, "men")
+  # end
+
 end
