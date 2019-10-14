b6b284eb62bd66a3db5362a5810ae2d3d81d079c
Gary Chen <garychen.whc@gmail.com>
2019-10-14 20:41:30 +0000

    added valiade for clothes

--- lib/store.rb
+++ lib/store.rb
@@ -2,11 +2,12 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true }, presence: true
-  # validate :validate_apparel
+  validate :validate_apparel
 
-  # def validate_apparel
-  #   if mens_apparel == false
-  #     errors.add(:mens_apparel, "men")
-  # end
+  def validate_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "Need some clothes!")
+    end
+  end
 
 end
